712cb606b2673498e214bf404220d3cffc6dfecf
samuel3310 <awoluowolabisamuel@gmail.com>
2024-05-23 08:36:53 +0100

    built

--- teledoct/src/pages/dashboard/index.jsx
+++ teledoct/src/pages/dashboard/index.jsx
@@ -14,18 +14,18 @@ const Dashboard = () => {
       id: 1,
 
       name: "Agent",
-      to: "/signup",
+      to: "/signin",
     },
     {
       id: 2,
 
       name: "Patient",
-      to: "/signup",
+      to: "/signin",
     },
     {
       id: 3,
       name: "Doctor",
-      to: "/signup",
+      to: "/signin",
     },
   ];
   return (
@@ -44,7 +44,7 @@ const Dashboard = () => {
               data-aos-duration={`${
                 id === 1 ? "1000" : id === 2 ? "2000" : "3000"
               }`}
-              to="/signin"
+              to={to}
               key={name}
               className="p-4 text-center bg-green-500 rounded-md text-white  border-2 border-green-500 transition-[background] duration-300 overflow-hidden relative after:contents-[''] after:absolute after:w-full after:h-2 after:bg-green-600  shadows shadow-orange-300  after:-left-0 after:-bottom-0 after:-right-0 
               font-semibold font-md
